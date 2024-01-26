3f2af133adbcebc8c3a43c931c115e5295b7732a
suhail k <wwwsuhail883@gmail.com>
2024-01-26 15:59:40 +0530

    likrd

--- Algorithem/binary-recursive.js
+++ Algorithem/binary-recursive.js
@@ -19,3 +19,51 @@ function search(arr,targat,leftIndex,rightIndex){
 
 }
 console.log(binaryRecasive([-5,2,4,6,10],10));
+
+
+
+
+
+function BainrySearch(arr,targat){
+return BainrySearch(arr,targat,0,arr.length-1)
+}
+function search(arr,targat,leftIndex,rightIndex){
+if(leftIndex<=rightIndex){
+    return -1
+}
+let mid= Math.floor((leftIndex+rightIndex)/2)
+if(targat===arr[mid]){
+    return mid
+}
+if(targat<arr[mid]){
+    return search(arr,targat,leftIndex,mid-1)
+}else{
+    return search(arr,targat,mid+1,rightIndex)
+}
+}
+console.log(BainrySearch([-5,2,4,6,10],10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- likedList/array-to-liked.js
+++ likedList/array-to-liked.js
@@ -41,3 +41,4 @@ class Node {
   
   singlyLinkedList.print();
   
+  
