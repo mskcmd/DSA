24b604c48c3a1b8805a09cbccc3f9d52f6332964
suhail k <wwwsuhail883@gmail.com>
2024-02-06 14:47:53 +0530

    sort

---
+++ Ds-Tow/instionsort.js
@@ -0,0 +1,13 @@
+function insrtionSort(arr){
+    for(let i=1;i<arr.length;i++){
+       let key=arr[i]
+       j=i-1;
+       while(j>=0&&arr[j]>key){
+        arr[j+1]=arr[j]
+        j--
+       }
+       arr[j+1]=key
+    }
+return arr
+}
+console.log(insrtionSort([5,4,3,2,1]))
