5cfbb7af8d35a7fe31dc943f772532a40021aa3a
suhail k <wwwsuhail883@gmail.com>
2024-02-28 17:59:32 +0530

    tree

---
+++ js.js
@@ -0,0 +1,10 @@
+function sumNumbers(n) {
+    if (n === 1) {
+        return 1; // Base case: when n is 1, return 1
+    } else {
+        return n + sumNumbers(n - 1); // Recursive case: add n to the sum of numbers from 1 to n-1
+    }
+}
+
+const result = sumNumbers(100000);
+console.log(result);
